65d60d87b5bdda627d928147d0d9a9b6aeadb404
codegangsk <kjhj0604@gmail.com>
2020-08-27 21:21:51 +0900

    first commit

--- 3A.3FindingFlatSurfaces/ViewController.swift
+++ 3A.3FindingFlatSurfaces/ViewController.swift
@@ -10,66 +10,80 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+final class ViewController: UIViewController {
 
-    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet private var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = [.vertical, .horizontal]
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
+}
 
-    // MARK: - ARSCNViewDelegate
+extension ViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        if planeAnchor.alignment == .horizontal {
+        let box = createBox(planeAnchor: planeAnchor)
+        node.addChildNode(box)
+        }
+        
+        if planeAnchor.alignment == .vertical {
+        let wall = createWall(planeAnchor: planeAnchor)
+        node.addChildNode(wall)
+    }
+    }
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        
+        for node in node.childNodes {
+                node.position = SCNVector3(planeAnchor.center.x, 0, planeAnchor.center.z)
+            if let plane = node.geometry as? SCNPlane {
+                plane.width = CGFloat(planeAnchor.extent.x)
+                plane.height = CGFloat(planeAnchor.extent.z)
+            }
+        }
+    }
+}
+
+extension ViewController {
+    private func createWall(planeAnchor: ARPlaneAnchor) -> SCNNode {
         let node = SCNNode()
-     
+        let geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        node.geometry = geometry
+        node.eulerAngles.x = -Float.pi / 2
+        node.opacity = 0.8
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        
         return node
     }
-*/
+    private func createBox(planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let node = SCNNode()
+        let geometry = SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0.0)
+        node.geometry = geometry
+        node.eulerAngles.x = -Float.pi / 2
+        node.opacity = 0.8
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
+    return node
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+}
 }
